55ddb7250d26151eeac6034dcfb16b0aff219e48
dibusan <dev.eriel.mf@gmail.com>
2018-11-21 00:36:51 -0500

    Added logginf system

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 ### To Deploy in Production (Ubuntu)
 - https://jonathanmh.com/deploying-go-apps-systemd-10-minutes-without-docker/
 1. Clone repo to Ubuntu server `$ git clone https://github.com/dibusan/scrape-free-proxy-list.git`
-2. Run installer `$ bash scrape-free-proxy-list/daemon/install.sh`
+2. Update `/lib/systemd/system/scrapefreeproxylist.service` line `ExecStart=/home/erieljr1/scrape-free-proxy-list` to reflect the correct path for the downloaded application
+3. Run installer `$ bash scrape-free-proxy-list/daemon/install.sh`
--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 	"regexp"
 	"strconv"
 	"encoding/json"
+	"time"
+	"log"
 )
 
 const (
@@ -46,28 +48,99 @@ func (p Proxy) String() string {
 		p.IP, p.Port, p.Code, p.Country, p.Anonymity, p.Google, p.HTTPS, p.LastChecked)
 }
 
-func getPage() []byte{
+const (
+	ERROR	=  iota // 0
+	WARNING			// 1
+	INFO			// 2
+	DEBUG			// 3
+	VERBOSE			// 4
+)
+
+var (
+	runtimeLog *os.File
+	logger *log.Logger
+	logLevel = ERROR
+)
+
+func initLogger(ll int){
+	logLevel = ll
+	runtimeLog, err := os.OpenFile("/var/log/scrapefreeproxylist.log",
+		os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Printf("error opening file: %v", err)
+		os.Exit(1)
+	}
+	logger = log.New(runtimeLog, "applog: ", log.Lshortfile|log.LstdFlags)
+
+}
+
+func logError(msg string){
+	logger.Println("ERROR: " + msg + "\nExit Status 1")
+}
+
+func logWarning(msg string){
+	if(logLevel >= WARNING){
+		logger.Println("WARNING: " + msg)
+	}
+}
+
+func logInfo(msg string){
+	if(logLevel >= INFO){
+		logger.Println("INFO: " + msg)
+	}
+}
+
+func logDebug(msg string){
+	if(logLevel >= DEBUG){
+		logger.Println("DEBUG: " + msg)
+	}
+}
+
+func logVerbose(msg string){
+	if(logLevel == VERBOSE){
+		logger.Println("VERBOSE: " + msg)
+	}
+}
+
+func getPage() ([]byte, error){
+	logInfo("Getting Free Proxy List webpage over HTTP")
 	u, err := url.Parse(freeProxyListURL)
-	if err != nil { panic(err) }
+	if err != nil {
+		logError("Failed to parse URL " + freeProxyListURL)
+		os.Exit(1)
+	}
 
 	r, err := http.Get(u.String())
-	if err != nil { panic(err) }
+	if err != nil {
+		logError("Failed HTTP Request. Err: " + err.Error())
+		return nil, err
+	}
 
 	// Save response body in []byte so it may be reused
 	pageBytes, err := ioutil.ReadAll(r.Body)
-	if err != nil { panic(err) }
+	if err != nil {
+		logError("Failed to read HTTP Response. Err: " + err.Error())
+		return nil, err
+	}
 
-	return pageBytes
+	logVerbose("Successfully retrieved Free Proxy List page contents.")
+	return pageBytes, nil
 }
 
 func savePage(body []byte){
 	err := os.MkdirAll(htmlPageStorage, 0777)
-	if err != nil { panic(err) }
+	if err != nil {
+		logWarning("Failed to make Html Page Directory. Err: " + err.Error())
+		return
+	}
 
 	err = ioutil.WriteFile(htmlFilePath, body, 0777)
-	if err != nil { panic(err) }
+	if err != nil {
+		logWarning("Failed to save HTML body. Err: " + err.Error())
+		return
+	}
 
-	fmt.Printf("Saved at %s\n", htmlFilePath)
+	logVerbose("Saved HTML body at " + htmlFilePath)
 }
 
 func containsId(n *html.Node, id string) bool {
@@ -99,6 +172,7 @@ func parseTableCell(tCell *html.Node) string{
 }
 
 func parseTableRow(tRow *html.Node) Proxy{
+	logInfo("Parse Row")
 
 	nProxy := Proxy{}
 	index := 0
@@ -136,6 +210,7 @@ func parseTableRow(tRow *html.Node) Proxy{
 }
 
 func parseTableBody(tBody *html.Node) []Proxy{
+	logInfo("Extract proxies from table body")
 	var proxies []Proxy
 	for c := tBody.FirstChild; c != nil; c = c.NextSibling {
 		if c.Type == html.ElementNode && c.Data == "tr" {
@@ -146,36 +221,51 @@ func parseTableBody(tBody *html.Node) []Proxy{
 }
 
 func parseTable(table *html.Node) []Proxy{
+	logInfo("Parse table")
 	for c := table.FirstChild; c != nil; c = c.NextSibling {
 		if c.Type == html.ElementNode && c.Data == "tbody" {
 			return parseTableBody(c)
 		}
 	}
+	logWarning("Table body not found.")
+	//TODO: logNotify
+	//logNotify("Page structure might have changed", pageBytes, err)
+
 	return []Proxy{}
 }
 
-func findTableNode(pageBytes []byte) (tNode *html.Node){
+func findTableNode(pageBytes []byte) (tNode *html.Node, err error){
+	logInfo("Finding proxies table node")
 	reader := bytes.NewReader(pageBytes)
 
 	doc, err := html.Parse(reader)
-	if err != nil { panic(err) }
+	if err != nil {
+		logError("Failed to parse HTML. Err: " + err.Error())
+		return
+	}
 
 	var findTable func(*html.Node) bool
 	findTable = func(n *html.Node) bool{
 		if n.Type == html.ElementNode && n.Data == "table" && containsId(n, "proxylisttable") {
+			logVerbose("Found table node with id='proxylisttable'")
 			tNode = n
 			return true
 		}
+		logVerbose("Iterate over all children of current node")
 		for c := n.FirstChild; c != nil; c = c.NextSibling{
 			if findTable(c) {
+				logVerbose("Table already found. Stopping subsequent iterations")
 				// This return prevents a unnecessary checks once the table has been found
 				return true
 			}
 		}
+		logVerbose("Table not found at this level")
 		return false
 	}
+	logVerbose("Kickoff recursive lookup for table")
 	findTable(doc) // if this is true, it found the table. TODO: Test it
 
+	logVerbose("Table lookup finished")
 	return
 }
 
@@ -224,47 +314,109 @@ func isUpdateComment(comment string) bool {
 }
 
 func deleteAllProxies() bool {
+	// TODO: Deleting all is a lazy cleanup approach. Redesign!
+	logInfo("Delete all existing proxies")
 	req, err := http.NewRequest(http.MethodDelete, deleteProxyBatchURL, nil)
-	if err != nil { panic(err) }
+	if err != nil {
+		logError("Failed to build request. Err: " + err.Error())
+		// TODO: logNotify
+		return false
+	}
 	client := http.Client{}
 	resp, err := client.Do(req)
-	if err != nil { panic(err) }
+	if err != nil {
+		logError("Failed to perform request. Err: " + err.Error())
+		// TODO: logNotify
+		return false
+	}
 
-	fmt.Println(resp.StatusCode)
+	logVerbose("Response Status: " + resp.Status)
 	return resp.StatusCode == http.StatusNoContent
 }
 
-func saveProxies(proxies []Proxy)  {
+func saveProxies(proxies []Proxy) bool {
+	logInfo("Save batch of proxies")
 	batch := Batch{ Batch: proxies }
 	b, err := json.Marshal(batch)
 	//b, err := json.MarshalIndent(proxies, "", " ")
 	if err != nil {
-		panic(err)
+		logError("Failed to Marshall list of Proxies. Err: " + err.Error())
+		// TODO: logNotify
+		return false
 	}
 
 	req, err := http.NewRequest(http.MethodPost, saveProxyBatchURL, bytes.NewBuffer(b))
+	if err != nil {
+		logError("Failed to build Post Request. Err: " + err.Error())
+		return false
+	}
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
-	if err != nil { panic(err) }
+	if err != nil {
+		logError("Failed to perform Post Request. Err: " + err.Error())
+		return false
+	}
 	defer resp.Body.Close()
 
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response Body:", string(body))
+	logVerbose("Response Status: " + resp.Status)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logVerbose("Failed to read Response Body. Err: " + err.Error())
+		return false
+	}
+	logVerbose("Response Body: " + string(body))
+	return true
 }
 
 func main() {
-	pageBytes := getPage()
-	savePage(pageBytes)
-	tableNode := findTableNode(pageBytes)
-	proxies := parseTable(tableNode)
+	initLogger(VERBOSE)
+	for {
+		pageBytes, err := getPage()
+		if err != nil {
+			logWarning("Failed to getPage. Sleep for 2 minutes and retry.")
+			time.Sleep(2 * time.Minute)
+			continue
+		}
 
-	if deleteAllProxies() {
-		saveProxies(proxies)
-	}else {
-		fmt.Printf("Could not clean up. Halting All inserts until DELETE is fixed")
+		savePage(pageBytes)
+		tableNode, err := findTableNode(pageBytes)
+		if err != nil {
+			logWarning("Failed to find table in page. Sleep for 2 minutes and retry.")
+
+			//TODO: Implement logNotify with logs an important Error/Warning along with relevant data and sends notifications
+			//logNotify("Page structure might have changed", pageBytes, err)
+
+			time.Sleep(2 * time.Minute)
+			continue
+		}
+
+		proxies := parseTable(tableNode)
+
+		if deleteAllProxies() {
+			saveProxies(proxies)
+		}else {
+			fmt.Printf("Could not clean up. Halting All inserts until DELETE is fixed")
+		}
+
+		logInfo("Sleeping 5 minutes...")
+		ticker := time.NewTicker(10 * time.Second)
+		//totalSleepSeconds := 5 * 60;
+		//elapsedSeconds := 0
+		go func() {
+			for t := range ticker.C {
+				//elapsedSeconds += 10
+
+				
+
+
+				logVerbose("Countdown: " + t.String() + " seconds...")
+
+				//logVerbose("Countdown: " + strconv.Itoa(totalSleepSeconds - t.Second()) + " seconds...")
+			}
+		}()
+		time.Sleep(5 * time.Minute)
+		ticker.Stop()
 	}
 }
